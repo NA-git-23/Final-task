b266befdca7c5a1911edb5bc02ac983f694dae6c
NA <nabdraim@gmail.com>
2023-03-09 13:06:37 +0600

    init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿ string[] s;
+            Console.WriteLine("Введите массив строк через пробел:");
+            string stroka = Console.ReadLine();
+            s = stroka.Split(' ');
+            var result = new string[s.Length];
+            var realSize = 0;
+            
+            foreach (var value in s )
+            {
+                if (value.Length <= 3)
+                
+                    {
+                    result[realSize] = value;
+                    realSize++;
+                    }
+            }
+            Console.WriteLine ($"Массив из строк, длина которых <= 3 символа: \n{string.Join(Environment.NewLine, result, 0, realSize)}");
+            
+            
